ea079a98db1e6f26b0313cd6fde1fd37c54af4e6
Egor Vdovenko <egor.vdovenko@roskvartal.ru>
2021-12-25 11:27:41 +0300

    feat: files controller

---
+++ demo-web-api/Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace demo_web_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private IWebHostEnvironment _hostingEnvironment;
+
+        public FilesController(IWebHostEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            string uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+
+            if (file.Length > 0)
+            {
+                string filePath = Path.Combine(uploads, file.FileName);
+				using Stream fileStream = new FileStream(filePath, FileMode.Create);
+				await file.CopyToAsync(fileStream);
+			}
+
+            return Ok();
+        }
+    }
+}
